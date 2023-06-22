16953d020516ee7c838674a0183da8e47dc2d31a
yuya1191994 <51263536+yuya1191994@users.noreply.github.com>
2023-06-22 16:04:42 +0900

    Optional Chaining

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,21 @@ import { App } from "./components/App";
 
 const rootElement = document.getElementById('root')
 render(<App />, rootElement)
+
+interface User {
+  name: string
+  sotial?: {
+    facebook: boolean
+    twitter: boolean
+  }
+}
+
+let user: User
+
+user = {name: 'Yuya', sotial: {facebook: true, twitter: true}}
+
+console.log(user)
+
+user = {name: 'Yuya'}
+
+console.log(user.sotial?.facebook)
